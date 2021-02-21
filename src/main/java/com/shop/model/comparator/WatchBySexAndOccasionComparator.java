8535c075c998291d5f39da2ca54658802d86d238
Alexandra Simonova <mazukata@gmail.com>
2021-02-22 02:03:49 +0300

    refactor code

--- src/main/java/com/shop/model/comparator/WatchBySexAndOccasionComparator.java
+++ src/main/java/com/shop/model/comparator/WatchBySexAndOccasionComparator.java
@@ -7,7 +7,7 @@ import java.util.Comparator;
 public class WatchBySexAndOccasionComparator implements Comparator<Watch> {
     @Override
     public int compare(Watch watch1, Watch watch2) {
-        int watchComparator = watch1.getSex().compareTo(watch1.getSex());
+        int watchComparator = watch1.getSex().compareTo(watch2.getSex());
         return ((watchComparator == 0) ? watch1.getOccasion().compareTo(watch2.getOccasion()) : watchComparator);
     }
 }
