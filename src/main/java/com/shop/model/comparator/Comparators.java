097f220d56292b8c79bb924c34b9df1c5295ffed
Alexandra Simonova <mazukata@gmail.com>
2021-02-21 23:22:43 +0300

    refactor code

--- src/main/java/com/shop/model/comparator/Comparators.java
+++ src/main/java/com/shop/model/comparator/Comparators.java
@@ -9,7 +9,7 @@ public class Comparators {
     public static final Comparator<Watch> byDateComparator = Comparator.comparing(Watch::getDate);
     public static final Comparator<Watch> byGlassMaterialComparator = Comparator.comparing(Watch::getGlassMaterial);
     public static final Comparator<Watch> byOccasionComparator = Comparator.comparing(Watch::getOccasion);
-    public static final Comparator<Watch> byOriginCountryComparator = Comparator.comparing(Watch::getOriginCountry);
-    public static final Comparator<Watch> byPriceComparator = Comparator.comparing(Watch::getPrice);
+    public static final Comparator<Watch> byOriginCountryComparator = Comparator.comparing(Watch::getOriginCountry).reversed();
+    public static final Comparator<Watch> byPriceComparator = Comparator.comparing(Watch::getPrice).reversed();
     public static final Comparator<Watch> bySexComparator = Comparator.comparing(Watch::getSex);
 }
